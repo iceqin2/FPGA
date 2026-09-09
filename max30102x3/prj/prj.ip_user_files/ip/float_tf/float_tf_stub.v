5d2c1b8e08990476dfda6be357cf1202c992566c
iceqin2 <qinj6195@gmail.com>
2026-09-09 16:14:10 +0800

    Add files via upload

---
+++ max30102x3/prj/prj.ip_user_files/ip/float_tf/float_tf_stub.v
@@ -0,0 +1,26 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+// Date        : Tue Jan  6 21:11:55 2026
+// Host        : pioneer running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub D:/vivado2/max30102x3/prj/prj.runs/float_tf_synth_1/float_tf_stub.v
+// Design      : float_tf
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a75tfgg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "floating_point_v7_1_9,Vivado 2019.2" *)
+module float_tf(aclk, s_axis_a_tvalid, s_axis_a_tready, 
+  s_axis_a_tdata, m_axis_result_tvalid, m_axis_result_tready, m_axis_result_tdata)
+/* synthesis syn_black_box black_box_pad_pin="aclk,s_axis_a_tvalid,s_axis_a_tready,s_axis_a_tdata[31:0],m_axis_result_tvalid,m_axis_result_tready,m_axis_result_tdata[31:0]" */;
+  input aclk;
+  input s_axis_a_tvalid;
+  output s_axis_a_tready;
+  input [31:0]s_axis_a_tdata;
+  output m_axis_result_tvalid;
+  input m_axis_result_tready;
+  output [31:0]m_axis_result_tdata;
+endmodule
---
+++ max30102x3/prj/prj.ip_user_files/ip/float_tf/float_tf_stub.vhdl
@@ -0,0 +1,35 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+-- Date        : Tue Jan  6 21:11:55 2026
+-- Host        : pioneer running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub D:/vivado2/max30102x3/prj/prj.runs/float_tf_synth_1/float_tf_stub.vhdl
+-- Design      : float_tf
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a75tfgg484-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity float_tf is
+  Port ( 
+    aclk : in STD_LOGIC;
+    s_axis_a_tvalid : in STD_LOGIC;
+    s_axis_a_tready : out STD_LOGIC;
+    s_axis_a_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    m_axis_result_tvalid : out STD_LOGIC;
+    m_axis_result_tready : in STD_LOGIC;
+    m_axis_result_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end float_tf;
+
+architecture stub of float_tf is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "aclk,s_axis_a_tvalid,s_axis_a_tready,s_axis_a_tdata[31:0],m_axis_result_tvalid,m_axis_result_tready,m_axis_result_tdata[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "floating_point_v7_1_9,Vivado 2019.2";
+begin
+end;
